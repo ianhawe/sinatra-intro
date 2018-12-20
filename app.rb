9b407156ae2ed75594e321c657bd55c917dd913f
Ian <ianhawe24@hotmail.co.uk>
2018-12-20 00:43:04 +0000

    routes made, post put delete used

---
+++ app.rb
@@ -0,0 +1,20 @@
+require "sinatra"
+# require "sinatra/contrib"
+
+# Create routes
+get "/" do "Hello world!" end
+get '/book' do "Hello book world!" end
+get '/movie' do "Hello movie world!" end
+get '/tech' do "Hello tech world!" end
+
+post "/tech" do "hello world2" end
+# Create all the 7 things 
+# localhost:4567
+# 
+# INDEX
+# from one thing to another
+# 
+# BOOK, MOVIE, TECH
+# Creativity for extra points
+# Time due:  Repo name:
+# post put delete has to be made
